3b0ac691a9742495af1cce6a2aca0a3a4ab9d233
Rodrigo Martins <56765781+rooxalot-dev@users.noreply.github.com>
2024-09-03 23:51:00 +0000

    fix: modifica mensagem da pagina principal

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Home() {
   return (
     <div className="home-container">
-      <h1>Daora o meu Tabnews!</h1>
+      <h1>Daora o Tabnews Clonagens!</h1>
     </div>
   );
 }
